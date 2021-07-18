f85540881b734e57af6a881580b091aac1909f45
Benedicta <nnennaanih1@gmail.com>
2021-07-18 09:48:48 +0300

    image test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -73,7 +73,7 @@ Header.Feature = function HeaderFeature({ children, ...restProps }) {
 };
 
 Header.Picture = function HeaderPicture({ src, ...restProps }) {
-  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+  return <Picture {...restProps} src={`public\images\users\${src}.png`} />;
 };
 
 Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
